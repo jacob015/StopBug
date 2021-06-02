21f9ce13177d64d80ace2ad8802e56aa27bcce99
jacob015 <66816737+jacob015@users.noreply.github.com>
2021-06-02 22:21:52 +0900

    Create Example2.cpp
    이 레포지토리의 경로 제 기준 D:\StopBug\StopBug에 모든 파일이 저장이 되있습니다. 이 곳에 파일을 새로 올리면 깃 허브 데스크탑의 commit to main 버튼이 활성화가 됩니다. 커밋을 누르고 오른쪽 상단에 Push ~~ 라고 뜨는데 누르면 업로드가 완료됩니다.

---
+++ Example2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	cout << "이 레포지토리의 경로 제 기준 D:\StopBug\StopBug에 모든 파일이 저장이 되있습니다. 이 곳에 파일을 새로 올리면";
+	cout << "깃 허브 데스크탑의 commit to main 버튼이 활성화가 됩니다. 커밋을 누르고 오른쪽 상단에 Push ~~ 라고 뜨는데 누르면 업로드가 완료됩니다.";
+}
